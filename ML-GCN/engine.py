c93dc003615ef171c8501fa08d0a8cfeb64fff01
tkhabia <tanmay.khabia@students.iiit.ac.in>
2022-05-03 19:43:26 +0530

    testing

--- ML-GCN/engine.py
+++ ML-GCN/engine.py
@@ -116,7 +116,8 @@ class Engine(object):
             target_var.volatile = True
 
         # compute output
-        self.state['output'] = model(input_var)
+        self.state['output'] , att = model(input_var)
+        torch.save(att , f"attention {input_var[1]}.pt")
         self.state['loss'] = criterion(self.state['output'], target_var)
 
         if training:
